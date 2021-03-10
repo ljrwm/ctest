2df82957c0129bb405b82dc1a036fa1603651571
ljrwm <liujingru_2200@126.com>
2021-03-10 21:36:13 +0800

    2021-3-10

--- 2021-3-10/test.c
+++ 2021-3-10/test.c
@@ -75,28 +75,105 @@
 //}
 //
 
-int main()
-{
-    int n, i, j;
-    while (scanf("%d", &n) != EOF)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j < n + 1 - i; j++)
-            {
-                printf("* ");
-            }
-            printf("\n\n");
-        }
-        for (i = 0; i < n; i++)
-        {
-            for (j = 0; j < i + 2; j++)
-            {
-                printf("* ");
-            }
-            printf("\n \n");
-        }
-        printf("\n");
-    }
-    return 0;
-}
+//int main()
+//{
+//    int n, i, j;
+//    while (scanf("%d", &n) != EOF)
+//    {
+//        for (i = 0; i <= n; i++)
+//        {
+//            for (j = 0; j < n + 1 - i; j++)
+//            {
+//                printf("* ");
+//            }
+//            printf("\n\n");
+//        }
+//        for (i = 0; i < n; i++)
+//        {
+//            for (j = 0; j < i + 2; j++)
+//            {
+//                printf("* ");
+//            }
+//            printf("\n \n");
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    int n, i, j, k;
+//    while (scanf("%d", &n) != EOF)
+//    {
+//        for (i = 1; i <= n; i++)
+//        {
+//            for (k = 1; k <= (n - i + 1) * 2; k++)
+//            {
+//                printf(" ");
+//            }
+//            for (j = 0; j < i; j++)
+//            {
+//                printf("*");
+//            }
+//            printf("\n");
+//        }
+//        for (i = 0; i <= n; i++)
+//        {
+//            for (k = 0; k < i * 2; k++)
+//            {
+//                printf(" ");
+//            }
+//            for (j = 0; j < (n - i + 1); j++)
+//            {
+//                printf("*");
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    int n, i, j;
+//    while (scanf("%d", &n) != EOF)
+//    {
+//        for (i = 0; i < n; i++)
+//        {
+//            for (j = 0; j < i; j++)
+//            {
+//                printf(" ");
+//            }
+//            printf("*");
+//            for (j = 0; j < n - i - i; j++)
+//            {
+//                printf(" ");
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    int n = 0;
+//    while (scanf("%d", &n) != EOF)
+//    {
+//        int i = 0;
+//        for (i = 0; i < n; i++)
+//        {
+//            int j = 0;
+//            for (j = 0; j < n; j++)
+//            {
+//                if (i == j)
+//                    printf("*");
+//                else
+//                    printf(" ");
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
